353a74d129a2cde742a8c974d0e0d2186a9710d7
PURPLE <9707.hyeon@gmail.com>
2021-02-02 19:06:36 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name : str
+    description : str = None
+    price : float
+    tax : float = None
+
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message":"Hello World"}
+
+@app.get("/item")
+async def create_item(item: Item):
+    return item
