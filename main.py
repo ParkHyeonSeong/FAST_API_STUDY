4d32053adc46cb2d5a942faa3a131976754e7f84
PURPLE <9707.hyeon@gmail.com>
2021-02-03 13:05:10 +0900

    Update main.py
    Login implement

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from urllib import parse
 
-class Item(BaseModel):
-    name : str
-    description : str = None
-    price : float
-    tax : float = None
+import auth
 
 app = FastAPI()
-@app.get("/")
-async def root():
-    return {"message":"Hello World"}
+app.mount("/static", StaticFiles(directory="static"), name = "static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/item")
-async def create_item(item: Item):
-    return item
+@app.get('/', response_class=HTMLResponse)
+async def page(request : Request):
+    return templates.TemplateResponse("index.html", {"request":request})
+
+@app.get("/login/", response_class=HTMLResponse)
+async def login(request : Request):
+    return templates.TemplateResponse("login.html", {"request":request})
+
+@app.get("/login/auth", response_class=HTMLResponse)
+async def login_auth(request : Request, id : str, pwd : str):
+    info = {"id" : id, "pwd" : pwd}
+    result = auth.compare(info["id"], info["pwd"])
+    return result
+    # parse.urlencode(info) # 인코딩하여 값 전송
+    # templates.TemplateResponse("index.html", {"request":request, "id" : id})
